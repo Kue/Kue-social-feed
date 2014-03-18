af90b98e61e028a3d2289c0bff6e313bdc9d6d17
Kue <jongkid@gmail.com>
2014-03-18 22:06:06 +0700

    1
    1

--- js/app.js
+++ js/app.js
@@ -12,10 +12,10 @@ window.fbAsyncInit = function () {
     FB.getLoginStatus(function (e) {
       e.status === "connected" ? $.getJSON("https://graph.facebook.com/fql?q=SELECT%20uid%2C%20name%2C%20pic_square%20FROM%20user%20WHERE%20uid%20%3D%20me()%20OR%20uid%20IN%20(SELECT%20uid2%20FROM%20friend%20WHERE%20uid1%20%3D%20me())&access_token=" + e.authResponse.accessToken, function (e) {
         FB.Canvas.setSize()
-      }) : top.location.href = "https://www.facebook.com/dialog/oauth?client_id=" + FACEBOOK_APP_ID + "&redirect_uri=" + FACEBOOK_CALLBACK_URL + "&scope=friends_online_presence"
+      }) : top.location.href = "https://www.facebook.com/dialog/oauth?client_id=" + FACEBOOK_APP_ID + "&redirect_uri=" + FACEBOOK_CALLBACK_URL + "&scope=email,user_birthday,user_about_me"
     })
   };
-//  setInterval(e, 15e3);
+  setInterval(e, 15e3);
   e()
 };
 (function (e) {
@@ -26,4 +26,3 @@ window.fbAsyncInit = function () {
   r.src = "//connect.facebook.net/en_US/all.js";
   i.parentNode.insertBefore(r, i)
 })(document, "script", "facebook-jssdk");
-
